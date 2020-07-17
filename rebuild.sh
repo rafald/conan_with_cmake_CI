a342d8510747d80819f6573208fb093522d46178
rafall <rdzbek@gmail.com>
2020-07-17 17:50:15 +0200

    fix cmake tests

--- rebuild.sh
+++ rebuild.sh
@@ -10,7 +10,7 @@ rm -rf $BUILD_DIR
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 
-#conan install ..
+#NOW INVOKED FROM CMAKE $> conan install ..
 
 #/usr/bin/cmake --no-warn-unused-cli -DCMAKE_PROJECT_INCLUDE_BEFORE:STRING=$THIS_DIR/SetupConan.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/bin/g++ -DCMAKE_TOOLCHAIN_FILE=$BUILD_DIR/conan_paths.cmake -H$THIS_DIR -B$BUILD_DIR -G Ninja
 /usr/bin/cmake --no-warn-unused-cli -DCMAKE_PROJECT_INCLUDE_BEFORE:STRING=$THIS_DIR/SetupConan.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/bin/g++ -H$THIS_DIR -B$BUILD_DIR -G Ninja
@@ -19,5 +19,6 @@ cd $BUILD_DIR
 
 # the following option does not work with target test:  --build $THIS_DIR/build
 #TODO binaried are in bin/ but cmtest expects in ./
-echo /usr/bin/ctest --build $BUILD_DIR/bin --config Debug --target test --output-on-failure -- -j 10
+pwd
+/usr/bin/ctest --build $BUILD_DIR/bin --config Debug --target test --output-on-failure -- -j 10
 cd ..
