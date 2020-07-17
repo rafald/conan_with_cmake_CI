308424d763eeaa4d4bcfcdf4ac39cbd264569144
rafall <rdzbek@gmail.com>
2020-07-17 15:47:14 +0200

    refine rebuild_conan

--- rebuild_conan.sh
+++ rebuild_conan.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env sh
 
 THIS_DIR=$(realpath $(dirname $0))
-BUILD_DIR=build
+BUILD_DIR=$THIS_DIR/build
 
 export VERBOSE=1
 
-rm -rf $THIS_DIR/$BUILD_DIR
-mkdir -p $THIS_DIR/$BUILD_DIR
-cd $THIS_DIR/$BUILD_DIR
+
+rm -rf $BUILD_DIR
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
 
 conan install ..
 
-#/usr/bin/cmake --no-warn-unused-cli -DCMAKE_PROJECT_INCLUDE_BEFORE:STRING=$THIS_DIR/SetupConan.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/bin/g++ -DCMAKE_TOOLCHAIN_FILE=$THIS_DIR/$BUILD_DIR/conan_paths.cmake -H$THIS_DIR -B$THIS_DIR/$BUILD_DIR -G Ninja
-/usr/bin/cmake --no-warn-unused-cli -DCMAKE_PROJECT_INCLUDE_BEFORE:STRING=$THIS_DIR/SetupConan.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/bin/g++ -H$THIS_DIR -B$THIS_DIR/$BUILD_DIR -G Ninja
+#/usr/bin/cmake --no-warn-unused-cli -DCMAKE_PROJECT_INCLUDE_BEFORE:STRING=$THIS_DIR/SetupConan.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/bin/g++ -DCMAKE_TOOLCHAIN_FILE=$BUILD_DIR/conan_paths.cmake -H$THIS_DIR -B$BUILD_DIR -G Ninja
+/usr/bin/cmake --no-warn-unused-cli -DCMAKE_PROJECT_INCLUDE_BEFORE:STRING=$THIS_DIR/SetupConan.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/bin/g++ -H$THIS_DIR -B$BUILD_DIR -G Ninja
 
-/usr/bin/cmake --build $THIS_DIR/$BUILD_DIR --config Debug --target all -- -j 10
+/usr/bin/cmake --build $BUILD_DIR --config Debug --target all -- -j 10
 
 # the following option does not work with target test:  --build $THIS_DIR/build
 #TODO binaried are in bin/ but cmtest expects in ./
-echo /usr/bin/ctest --build $THIS_DIR/$BUILD_DIR/bin --config Debug --target test --output-on-failure -- -j 10
+echo /usr/bin/ctest --build $BUILD_DIR/bin --config Debug --target test --output-on-failure -- -j 10
 cd ..
