e4e54cee6ca45e03a5ed9ea81168bd039e817f7c
rafall <rdzbek@gmail.com>
2020-08-07 16:37:52 +0200

    enforce ninja builder

--- rebuild.sh
+++ rebuild.sh
@@ -8,18 +8,20 @@ export VERBOSE=1
 
 
 rm -rf $BUILD_DIR
-mkdir -p $BUILD_DIR
-cd $BUILD_DIR
+ninja --version
 
 #NOW INVOKED FROM CMAKE $> conan install ..
 
-#cmake --no-warn-unused-cli -DCMAKE_PROJECT_INCLUDE_BEFORE:STRING=$THIS_DIR/SetupConan.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/bin/g++ -DCMAKE_TOOLCHAIN_FILE=$BUILD_DIR/conan_paths.cmake -H$THIS_DIR -B$BUILD_DIR -G Ninja
-cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/bin/g++ -H$THIS_DIR -B$BUILD_DIR -G Ninja # -DCMAKE_PROJECT_INCLUDE_BEFORE:STRING=$THIS_DIR/InvokeConan.cmake
+#cmake --no-warn-unused-cli -DCMAKE_PROJECT_INCLUDE_BEFORE:STRING=$THIS_DIR/SetupConan.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/bin/g++ -DCMAKE_TOOLCHAIN_FILE=$BUILD_DIR/conan_paths.cmake -H$THIS_DIR -B$BUILD_DIR 
+cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/bin/g++ -H$THIS_DIR -B$BUILD_DIR 
+# -DCMAKE_PROJECT_INCLUDE_BEFORE:STRING=$THIS_DIR/InvokeConan.cmake
 
 cmake --build $BUILD_DIR --config Debug --target all -- -j 10
 
 # the following option does not work with target test:  --build $THIS_DIR/build
 #TODO binaried are in bin/ but cmtest expects in ./
+
+cd $BUILD_DIR
 pwd
 ctest --build $BUILD_DIR/bin --config Debug --target test --output-on-failure -- -j 10
 cd ..
