c1c143f90ed469b7f3ea380ea2b28781282695ec
rafald <rdzbek@gmail.com>
2022-03-29 21:20:49 +0200

    bump version no of conan.cmake

--- InvokeConan.cmake
+++ InvokeConan.cmake
@@ -2,7 +2,7 @@
 # Download automatically, you can also just copy the conan.cmake file
 if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
    message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-   file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.15/conan.cmake"
+   file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.18/conan.cmake"
                  "${CMAKE_BINARY_DIR}/conan.cmake")
 endif()
 
