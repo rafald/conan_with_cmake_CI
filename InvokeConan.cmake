f2b417fa979860b2133cbe4790e02da5e5c2fb51
rafald <rdzbek@gmail.com>
2022-04-01 20:19:04 +0200

    remove remote

--- InvokeConan.cmake
+++ InvokeConan.cmake
@@ -11,10 +11,6 @@ endif()
 
 include(${CMAKE_BINARY_DIR}/conan.cmake)
 conan_check(VERSION 1.0.0 REQUIRED)
-conan_add_remote(NAME bincrafters
-                 INDEX 1
-                 URL https://api.bintray.com/conan/bincrafters/public-conan
-                 VERIFY_SSL True)
 conan_cmake_run(CONANFILE conanfile.txt #REQUIRES boost/1.73.0 gtest/1.10.0
                 BASIC_SETUP #CMAKE_TARGETS
                 BUILD missing
